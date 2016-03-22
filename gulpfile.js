195284f16b5063932a26281260414350f9db0454
David <dstellini@nxpay.eu>
2016-03-22 11:34:09 +0100

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var WebpackDevServer = require("webpack-dev-server");
+var gulp = require('gulp');
+var KarmaServer = require('karma').Server;
+
+var cfg = {
+  _dir : './config',
+  karma : __dirname + '/config/karma.conf.js',
+  webpack : require('./config/webpack.dev')
+};
+
+var webpack = require('webpack');
+var compiler = webpack(cfg.webpack);
+
+var port_server = 8085;
+var port_db = 8086;
+
+//dev task:
+gulp.task('server', function(){
+    //Webpackdev server builds so there's no need
+    new WebpackDevServer(compiler, {
+      hot:true,
+      noInfo: false,
+      publicPath: cfg.webpack.output.publicPath
+    }).listen(port_server, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        // Server listening
+        console.log("Listening at http://localhost:" + port_server);
+      });
+});
+
+
+gulp.task('test', function(done){
+  new KarmaServer({
+    configFile: cfg.karma,
+    singleRun: true
+  }, done).start();
+});
